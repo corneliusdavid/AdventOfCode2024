b323e70250cbddab30de5f0631d04e6210b42bc1
David Cornelius <dev@corneliusconcepts.com>
2024-12-13 17:14:16 -0800

    Day 4 - finally got the XMAS count correct!!

--- 04-CeresSearch/XMASCount.dpr
+++ 04-CeresSearch/XMASCount.dpr
@@ -13,25 +13,157 @@ program XMASCount;
 uses
   System.SysUtils, System.Classes, System.IOUtils, System.Math, System.StrUtils, RegularExpressions;
 
+
+
 procedure GenerateAnswer(const InputLines: TArray<string>);
+
+  function CheckBackwardsXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (col >= 4) and (Copy(InputLines[row], col - 3, 4) = 'SAMX');
+  end;
+
+  function CheckUpLeftXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (col >= 4) and (row >= 3) and (InputLines[row-3][col-3] = 'S') and
+                                            (InputLines[row-2][col-2] = 'A') and
+                                            (InputLines[row-1][col-1] = 'M');
+  end;
+
+  function CheckUpVertXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (row >= 3) and (InputLines[row-3][col] = 'S') and
+                             (InputLines[row-2][col] = 'A') and
+                             (InputLines[row-1][col] = 'M');
+  end;
+
+  function CheckUpRightXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (col <= Length(InputLines[0]) - 3) and (row >= 3) and
+              (InputLines[row-3][col+3] = 'S') and
+              (InputLines[row-2][col+2] = 'A') and
+              (InputLines[row-1][col+1] = 'M');
+  end;
+
+  function CheckRightXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (col < Length(InputLines[0]) - 3) and (Copy(InputLines[row], col, 4) = 'XMAS');
+  end;
+
+  function CheckDownRightXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (col < Length(InputLines[0]) - 3) and (row < Length(InputLines) - 4) and
+              (InputLines[row+1][col+1] = 'M') and
+              (InputLines[row+2][col+2] = 'A') and
+              (InputLines[row+3][col+3] = 'S');
+  end;
+
+  function CheckDownVertXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (row < Length(InputLines) - 4) and
+              (InputLines[row+1][col] = 'M') and
+              (InputLines[row+2][col] = 'A') and
+              (InputLines[row+3][col] = 'S');
+  end;
+
+  function CheckDownLeftXMAS(const row, col: Integer): Boolean;
+  begin
+    Result := (row < Length(InputLines) - 4) and (col > 3) and
+              (InputLines[row+1][col-1] = 'M') and
+              (InputLines[row+2][col-2] = 'A') and
+              (InputLines[row+3][col-3] = 'S');
+  end;
+
+  function CountXMAS(const InputLine: string): Integer;
+  var
+    p, start_p: Integer;
+  begin
+    Result := 0;
+
+    start_p := 1;
+    repeat
+      p := Pos('XMAS', InputLine, start_p);
+      if p > 0 then begin
+        start_p := p + 4;
+        Inc(Result);
+      end;
+    until p = 0;
+  end;
+
+  function CountSAMX(const InputLine: string): Integer;
+  var
+    p, start_p: Integer;
+  begin
+    Result := 0;
+
+    start_p := 1;
+    repeat
+      p := Pos('SAMX', InputLine, start_p);
+      if p > 0 then begin
+        start_p := p + 4;
+        Inc(Result);
+      end;
+    until p = 0;
+  end;
+
 var
   HorzXMASCount: Integer;
   VertXMASCount: Integer;
   RtDiagXMASCount: Integer;
   LfDiagXMASCount: Integer;
+
   VertLines: TArray<string>;   // strings built up of characters in a vertial line through the grid
   RtDiagLines: TArray<string>; // strings built up of characters in a diagonal line down to the right
   LfDiagLines: TArray<string>; // strings built up of characters in a diagonal line down to the left
-  RegEx: TRegEx;
 begin
   HorzXMASCount := 0;
   VertXMASCount := 0;
   RtDiagXMASCount := 0;
   LfDiagXMASCount := 0;
 
-  RegEx := TRegEx.Create('(XMAS)|(SAMX)');
-
   Writeln(Format('The Input file has %d rows, each of which is %d characters long.', [Length(InputLines), Length(InputLines[0])]));
+
+  // This problem is attempted using two very different methods:
+  // 1. The first method looks at each character in the original array of strings and then compares the surrounding
+  // characters to see if they form "XMAS" in any direction. This is the most popular approach seen in posted answers.
+  for var row := 0 to Length(InputLines) - 1 do
+    for var col := 1 to Length(InputLines[0]) do
+      // if the current character is X...
+      if InputLines[row][col] = 'X' then begin
+        // .. check the characters in all directions to see if they spell "XMAS"
+        if CheckBackwardsXMAS(row, col) then
+          Inc(HorzXMASCount);
+        if CheckUpLeftXMAS(row, col) then
+          Inc(LfDiagXMASCount);
+        if CheckUpVertXMAS(row, col) then
+          Inc(VertXMASCount);
+        if CheckUpRightXMAS(row, col) then
+          Inc(RtDiagXMASCount);
+        if CheckRightXMAS(row, col) then
+          Inc(HorzXMASCount);
+        if CheckDownRightXMAS(row, col) then
+          Inc(LfDiagXMASCount);
+        if CheckDownVertXMAS(row, col) then
+          Inc(VertXMASCount);
+        if CheckDownLeftXMAS(row, col) then
+          Inc(RtDiagXMASCount);
+      end;
+
+  Writeln('METHOD ONE: search in all directions from each "X":');
+  Writeln(Format('Found %d cases of XMAS in horizontal lines,' + sLineBreak +
+                 '      %d cases of XMAS in vertial lines,' + sLineBreak +
+                 '      %d cases of XMAS in right-diagonals,' + sLineBreak +
+                 '      %d cases of XMAS in left-diagonal lines' + sLineBreak +
+                 '  for a total of %d cases of XMAS! (correct for small 10x10 grid)',
+                  [HorzXMASCount, VertXMASCount, RtDiagXMASCount, LfDiagXMASCount,
+                   HorzXMASCount + VertXMASCount + RtDiagXMASCount + LfDiagXMASCount]));
+
+
+  // Here's my original method, creating strings of all the vertical and diagonal lines,
+  // then using standard search through all these extracted strings to find "XMAS"
+  HorzXMASCount := 0;
+  VertXMASCount := 0;
+  RtDiagXMASCount := 0;
+  LfDiagXMASCount := 0;
 
   // set the number of vertical lines
   SetLength(VertLines, Length(InputLines[0]));
@@ -71,54 +203,42 @@ begin
   // build vertical lines
   for var col := 1 to Length(InputLines[0]) do
     for var row := 0 to Length(InputLines) - 1 do
-      VertLines[row] := VertLines[row] + InputLines[row][col];
+      VertLines[col-1] := VertLines[col-1] + InputLines[row][col];
+
+  // now that all the vertical and diagonal lines have been extracted, it's simple to find XMAS in them...
 
   // search horizontal lines
-  for var row := 0 to Length(InputLines) - 1 do begin
-    var p := RegEx.Match(InputLines[row]);
-    while p.Success do begin
-      Inc(HorzXMASCount);
-      p := p.NextMatch;
-    end;
-  end;
+  for var row := 0 to Length(InputLines) - 1 do
+    HorzXMASCount := HorzXMASCount + CountXMAS(InputLines[row]) + CountSAMX(InputLines[row]);
 
   // search vertical lines
-  for var i := 0 to Length(VertLines) - 1 do begin
-    var p := RegEx.Match(VertLines[i]);
-    while p.Success do begin
-      Inc(VertXMASCount);
-      p := p.NextMatch;
-    end;
-  end;
+  for var i := 0 to Length(VertLines) - 1 do
+    VertXMASCount := VertXMASCount + CountXMAS(VertLines[i]) + CountSAMX(VertLines[i]);
 
   // search downward-right diagonal lines
-  for var i := 0 to Length(RtDiagLines) - 1 do begin
-    var p := RegEx.Match(RtDiagLines[i]);
-    while p.Success do begin
-      Inc(RtDiagXMASCount);
-      p := p.NextMatch;
-    end;
-  end;
+  for var i := 0 to Length(RtDiagLines) - 1 do
+    RtDiagXMASCount := RtDiagXMASCount + CountXMAS(RtDiagLines[i]) + CountSAMX(RtDiagLines[i]);
 
   // search downward-left diagonal lines
-  for var i := 0 to Length(LfDiagLines) - 1 do begin
-    var p := RegEx.Match(LfDiagLines[i]);
-    while p.Success do begin
-      Inc(LfDiagXMASCount);
-      p := p.NextMatch;
-    end;
-  end;
+  for var i := 0 to Length(LfDiagLines) - 1 do
+    LfDiagXMASCount := LfDiagXMASCount + CountXMAS(LfDiagLines[i]) + CountSAMX(LfDiagLines[i]);
 
-  Writeln(Format('Found %d cases of XMAS in %d horizontal lines,' + sLineBreak +
-                 '      %d cases of XMAS in %d vertial lines,' + sLineBreak +
-                 '      %d cases of XMAS in %d right-diagonals,' + sLineBreak +
-                 '      %d cases of XMAS in %d left-diagonal lines' + sLineBreak +
+  Writeln('METHOD TWO: extract vertical and diagonal lines and then simply search through them all:');
+  Writeln(Format('Found %d cases of XMAS in horizontal lines,' + sLineBreak +
+                 '      %d cases of XMAS in vertial lines,' + sLineBreak +
+                 '      %d cases of XMAS in right-diagonals,' + sLineBreak +
+                 '      %d cases of XMAS in left-diagonal lines' + sLineBreak +
                  '  for a total of %d cases of XMAS!',
-                  [HorzXMASCount, Length(InputLines),
-                   VertXMASCount, Length(VertLines),
-                   RtDiagXMASCount, Length(RtDiagLines),
-                   LfDiagXMASCount, Length(LfDiagLines),
+                  [HorzXMASCount, VertXMASCount, RtDiagXMASCount, LfDiagXMASCount,
                    HorzXMASCount + VertXMASCount + RtDiagXMASCount + LfDiagXMASCount]));
+
+  (* NOTE: The only way this got the right answer was to add a layer of characters (e.g. "Q") around the
+   * entire input, a new row at the top and bottom, and an additional letter at the start and end of each
+   * row. This is the only way both answers agree and I finally got a right answer. After trying the original
+   * input, I discovered Method Two got the right answer--method one was quite short!
+   * This means my algorithms are off by one (or more) somewhere in Method One.  :-(
+   * However, I've spent a LOT of time on this puzzle; I'm not going to attempt part 2 or to fix this one.
+   *)
 end;
 
 function ParentPath: string;
