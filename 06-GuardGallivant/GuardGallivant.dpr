c4cbd76bb63fc8c67e193cb043db393b59a068f4
David Cornelius <dev@corneliusconcepts.com>
2024-12-16 08:35:43 -0800

    Day 6 - solved, first try!

--- 06-GuardGallivant/GuardGallivant.dpr
+++ 06-GuardGallivant/GuardGallivant.dpr
@@ -14,14 +14,116 @@ uses
   System.SysUtils, System.Classes, System.IOUtils, System.Math, System.StrUtils, RegularExpressions;
 
 procedure GenerateAnswer(const InputLines: TArray<string>);
-begin
-  // process each line
-  for var i := 0 to Length(InputLines) - 1 do begin
-    //var CurrLine := InputLines[i];
+type
+  TGuardDirections = (gdUp, gdDown, gdLeft, gdRight);
+  TGuardPosChars = set of Char;
+const
+  GuardPosChars: TGuardPosChars = ['^', 'v', '<', '>'];
+  ObstacleChar = '#';
 
+  function GuardLeftArea(const X, Y: Integer): Boolean;
+  begin
+    Result := (x < 1) or (x > Length(InputLines[0]))   // before or after edge of string
+           or (y < 0) or (y > Length(InputLines) - 1); // before or after edge of row array
   end;
 
-  Writeln(Format('No answer generated yet--but there are %d lines in the input file', [Length(InputLines)]));
+  function PosCharToDirection(const GuardPosChar: Char): TGuardDirections;
+  begin
+    if GuardPosChar = '^' then
+      Result := gdUp
+    else if GuardPosChar = 'v' then
+      Result := gdDown
+    else if GuardPosChar = '<' then
+      Result := gdLeft
+    else if GuardPosChar = '>' then
+      Result := gdRight;
+  end;
+
+  function GuardFacingObstacle(const X, Y: Integer; const Dir: TGuardDirections): Boolean;
+  begin
+    Result := False;
+
+    case Dir of
+      gdUp:
+        if (y > 0) and (InputLines[y-1][x] = ObstacleChar) then
+          Result := True;
+      gdDown:
+        if (y < Length(InputLines)-1) and (InputLines[y+1][x] = ObstacleChar) then
+          Result := True;
+      gdLeft:
+        if (x > 1) and (InputLines[y][x-1] = ObstacleChar) then
+          Result := True;
+      gdRight:
+        if (x < Length(InputLines[0])) and (InputLines[y][x+1] = ObstacleChar) then
+          Result := True;
+    end;
+  end;
+
+  procedure TurnGuardRight(var CurrGuardDir: TGuardDirections);
+  begin
+    case CurrGuardDir of
+      gdUp:
+        CurrGuardDir := gdRight;
+      gdDown:
+        CurrGuardDir := gdLeft;
+      gdLeft:
+        CurrGuardDir := gdUp;
+      gdRight:
+        CurrGuardDir := gdDown;
+    end;
+  end;
+
+var
+  GuardPosCount: Integer;
+  GuardPosX: Integer;   // 1-based: string
+  GuardPosY: Integer;   // 0-based: array
+  GuardDir: TGuardDirections;
+begin
+  GuardPosCount := 0;
+
+  // find starting guard position
+  GuardPosX := 0;
+  GuardPosY := -1;
+  for var row := 0 to Length(InputLines) - 1 do begin
+    for var col := 1 to Length(InputLines[0]) do
+      if CharInSet(InputLines[row][col], GuardPosChars) then begin
+        GuardPosX := col;
+        GuardPosY := row;
+        GuardDir := PosCharToDirection(InputLines[row][col]);
+        InputLines[row][col] := '.';
+        Break;
+      end;
+    if GuardPosX > 0 then
+      Break;
+  end;
+
+  Writeln(Format('starting guard position: %d, %d', [GuardPosX, GuardPosY]));
+
+  // move the guard position repeatedly until off the grid
+  repeat
+    if InputLines[GuardPosY][GuardPosX] = '.' then begin
+      Inc(GuardPosCount);
+      InputLines[GuardPosY][GuardPosX] := ' ';
+    end;
+
+    while GuardFacingObstacle(GuardPosX, GuardPosY, GuardDir) do
+      TurnGuardRight(GuardDir);
+
+    // move guard one step in current direction
+    case GuardDir of
+      gdUp:
+        Dec(GuardPosY);
+      gdDown:
+        Inc(GuardPosY);
+      gdLeft:
+        Dec(GuardPosX);
+      gdRight:
+        Inc(GuardPosX);
+    end;
+  until GuardLeftArea(GuardPosX, GuardPosY);
+
+
+  Writeln(Format('The guard was in %d distinct positions.', [GuardPosCount]));
 end;
 
 function ParentPath: string;
