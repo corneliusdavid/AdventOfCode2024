0d4ab4881d6520ac8ddccc5f6b42f664174e4198
David Cornelius <dev@corneliusconcepts.com>
2024-12-16 21:11:43 -0800

    Day 7 - solved

--- 07-BridgeRepair/BridgeRepair.dpr
+++ 07-BridgeRepair/BridgeRepair.dpr
@@ -14,14 +14,78 @@ uses
   System.SysUtils, System.Classes, System.IOUtils, System.Math, System.StrUtils, RegularExpressions;
 
 procedure GenerateAnswer(const InputLines: TArray<string>);
-begin
-  // process each line
-  for var i := 0 to Length(InputLines) - 1 do begin
-    var CurrLine := InputLines[i];
 
+  function StrsToNums(const NumStr: string): TArray<Int64>;
+  var
+    NumList: TStringList;
+  begin
+    NumList := TStringList.Create;
+    try
+      NumList.CommaText := NumStr;
+      SetLength(Result, NumList.Count);
+      for var n := 0 to NumList.Count - 1 do
+        Result[n] := NumList[n].ToInt64;
+    finally
+      NumList.Free;
+    end;
   end;
 
-  Writeln(Format('No answer generated yet--but there are %d lines in the input file', [Length(InputLines)]));
+var
+  EqAns: Int64;
+  CheckAnswer: Int64;
+  Numbers: TArray<Int64>;
+  EquationsCorrect: Integer;
+  TotalEqAnswers: Int64;
+begin
+  EquationsCorrect := 0;
+  TotalEqAnswers := 0;
+
+  // process each line
+  for var row := 0 to Length(InputLines) - 1 do begin
+    var CurrLine := Trim(InputLines[row]);
+    if not CurrLine.IsEmpty then begin
+      var CPos := Pos(':', CurrLine);
+      if CPos > 0 then begin
+        // save the equation answer
+        EqAns := StrToInt64(LeftStr(CurrLine, CPos - 1));
+        CurrLine := Trim(Copy(CurrLine, CPos + 1, CurrLine.Length));
+        // parse the rest of the line into a number array
+        Numbers := StrsToNums(CurrLine);
+
+        // number of permutations rises exponentially based on how many numbers we have
+        var TotalPerms := Round(IntPower(2, Length(Numbers)-1));
+
+        for var perm := 0 to TotalPerms-1 do begin
+          // initialize check answer for this permutation of operators
+          CheckAnswer := Numbers[0];
+          var GoodEq := EqAns.ToString + ' = ' + CheckAnswer.ToString;
+
+          // the binary bits of the current permutation (perm) will determine what operators are used
+          var bitperm := perm;
+          for var bit := 1 to Length(Numbers) - 1 do begin
+            if Odd(bitperm) then begin
+              CheckAnswer := CheckAnswer * Numbers[bit];
+              GoodEq := GoodEq + ' * ' + Numbers[bit].ToString;
+            end else begin
+              CheckAnswer := CheckAnswer + Numbers[bit];
+              GoodEq := GoodEq + ' + ' + Numbers[bit].ToString;
+            end;
+            bitperm := bitperm shr 1;
+          end;
+
+          if CheckAnswer = EqAns then begin
+            Inc(EquationsCorrect);
+            TotalEqAnswers := TotalEqAnswers + EqAns;
+            Writeln('Good equation: ' + GoodEq);
+            Break;
+          end;
+        end;
+      end;
+    end;
+  end;
+
+  Writeln(Format('There are %d equations in the input file, %d can be true for a sum total of %d.',
+                  [Length(InputLines), EquationsCorrect, TotalEqAnswers]));
 end;
 
 function ParentPath: string;
