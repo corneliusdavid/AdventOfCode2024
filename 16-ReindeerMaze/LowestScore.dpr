76a223b4d19ea3a97ce3a4a4ef6e31174a76acf0
David Cornelius <dev@corneliusconcepts.com>
2024-12-26 09:33:34 -0800

    stubbed out the rest of the days; no solutions yet

---
+++ 16-ReindeerMaze/LowestScore.dpr
@@ -0,0 +1,34 @@
+program LowestScore;
+(* as: LowestScore.exe, a Windows console app
+ * in: Delphi 12.2
+ * on: December, 2024
+ * by: David Cornelius
+ * to: Solve Day 16a of Advent of Code, 2024 (https://adventofcode.com/2024/day/16)
+ *)
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils, System.Classes, System.IOUtils, System.Math, System.StrUtils;
+
+procedure GenerateAnswer(const InputLines: TArray<string>);
+begin
+  var CurrLine := InputLines[0];
+
+  Writeln(Format('No answer yet; there are %d lines in the input file.',
+                 [Length(InputLines)]));
+end;
+
+function ParentPath: string;
+begin
+  // executables are created in a .\$(Platform)\$(Config) folder, so look two parents up for files
+  Result := '..' + TPath.DirectorySeparatorChar + '..';
+end;
+
+begin
+  Writeln('Day 16a of Advent of Code, 2024 (https://adventofcode.com/2024/day/16)');
+  GenerateAnswer(TFile.ReadAllLines(TPath.Combine(ParentPath, 'input.txt')));
+  Readln;
+end.
