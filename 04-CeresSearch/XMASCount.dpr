6160310d0a2902c923439465bb84b242b14a324a
David Cornelius <dev@corneliusconcepts.com>
2025-01-05 07:02:36 -0800

    Day 4 - finally finished part 2

--- 04-CeresSearch/XMASCount.dpr
+++ 04-CeresSearch/XMASCount.dpr
@@ -34,6 +34,9 @@ end;
 procedure GenerateAnswer(const XMASGrid: CharArray2D);
 const
   MAS_STR = 'MAS';
+var
+  maxX: Integer;
+  maxY: Integer;
 
   function StringFromCoords(y1, x1, y2, x2, y3, x3: Integer): string;
   begin
@@ -58,31 +61,31 @@ const
 
   function CheckUpRightXMAS(const row, col: Integer): Boolean;
   begin
-    Result := (row >= 2) and (col < Length(XMASGrid[0]) - 2) and
+    Result := (row >= 2) and (col < maxX - 2) and
               (StringFromCoords(row, col, row-1, col+1, row-2, col+2) = MAS_STR);
   end;
 
   function CheckRightXMAS(const row, col: Integer): Boolean;
   begin
-    Result := (col < Length(XMASGrid[0]) - 2) and
+    Result := (col < maxY - 2) and
               (StringFromCoords(row, col, row, col+1, row, col+2) = MAS_STR);
   end;
 
   function CheckDownRightXMAS(const row, col: Integer): Boolean;
   begin
-    Result := (col < Length(XMASGrid[0]) - 2) and (row < Length(XMASGrid) - 2) and
+    Result := (col < maxX - 2) and (row < maxY - 2) and
               (StringFromCoords(row, col, row+1, col+1, row+2, col+2) = MAS_STR);
   end;
 
   function CheckDownVertXMAS(const row, col: Integer): Boolean;
   begin
-    Result := (row < Length(XMASGrid) - 2) and
+    Result := (row < maxY - 2) and
               (StringFromCoords(row, col, row+1, col, row+2, col) = MAS_STR);
   end;
 
   function CheckDownLeftXMAS(const row, col: Integer): Boolean;
   begin
-    Result := (row < Length(XMASGrid) - 2) and (col >= 2) and
+    Result := (row < maxY - 2) and (col >= 2) and
               (StringFromCoords(row, col, row+1, col-1, row+2, col-2) = MAS_STR);
   end;
 
@@ -91,11 +94,15 @@ var
   VertXMASCount: Integer;
   RtDiagXMASCount: Integer;
   LfDiagXMASCount: Integer;
+  DiagCrossCount: Integer;
 begin
   HorzXMASCount := 0;
   VertXMASCount := 0;
   RtDiagXMASCount := 0;
   LfDiagXMASCount := 0;
+  DiagCrossCount := 0;
+  maxY := Length(XMASGrid);
+  maxX := Length(XMASGrid[0]);
 
   {$IFDEF DEBUG}
   Writeln(Format('The Input file has %d rows and %d columns.', [Length(XMASGrid), Length(XMASGrid[0])]));
@@ -104,8 +111,8 @@ begin
   // look at each character in the original array of strings and compare the surrounding
   // characters to see if they form "XMAS" in any direction.
   for var row := 0 to Length(XMASGrid) - 1 do
-    for var col := 0 to Length(XMASGrid[row]) - 1 do
-      // if the current character is X...
+    for var col := 0 to Length(XMASGrid[row]) - 1 do begin
+      // Part 1: if the current character is X...
       if XMASGrid[row][col] = 'X' then begin
         // .. check the characters in all directions to see if they spell "XMAS"
         if CheckBackwardsXMAS(row, col-1) then
@@ -126,6 +133,16 @@ begin
           Inc(RtDiagXMASCount);
       end;
 
+      // Part 2: if the current character is 'A' find "cross" matches of 'MAS' ...
+      if (XMASGrid[row][col] = 'A') and (row > 0) and (row < maxY-1) and (col > 0) and (col < maxX-1) then begin
+        // ... check diagonals for "MAS" in "X" formation
+        if (CheckUpLeftXMAS(row+1, col+1) or CheckDownRightXMAS(row-1, col-1)) and
+          (CheckUpRightXMAS(row+1, col-1) or CheckDownLeftXMAS(row-1, col+1)) then begin
+          Inc(DiagCrossCount);
+        end;
+      end;
+    end;
+
   {$IFDEF DEBUG}
   Writeln('Part 1: search in all directions from each "X":');
   Writeln(Format('Found %d cases of XMAS in horizontal lines,' + sLineBreak +
@@ -135,9 +152,11 @@ begin
                  '  for a total of %d cases of XMAS!',
                   [HorzXMASCount, VertXMASCount, RtDiagXMASCount, LfDiagXMASCount,
                    HorzXMASCount + VertXMASCount + RtDiagXMASCount + LfDiagXMASCount]));
+  Writeln(Format('Part 2: Found %d cases of crossed "MAS"', [DiagCrossCount]));
   {$ELSE}
   Writeln(Format('Part 1: Found a total of %d cases of XMAS!',
                   [HorzXMASCount + VertXMASCount + RtDiagXMASCount + LfDiagXMASCount]));
+  Writeln(Format('Part 2: Found %d cases of crossed "MAS"', [DiagCrossCount]));
   {$ENDIF}
 
 end;
